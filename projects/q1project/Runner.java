666bc0463888430461cfa42fefe873982980d6f7
Doggo252 <neilspatil@icloud.com>
2026-09-22 11:43:21 -0700

    9/22 classwork

--- projects/q1project/Runner.java
+++ projects/q1project/Runner.java
@@ -1,18 +1,31 @@
 import javax.swing.JFrame;
+import java.util.Scanner;
 
 // Runner that can be used with JPanel Graphics
 public class Runner {
 	public static void main( String args[] ) {
-        
         // Create the frame object. Give it a title appropriate to the application
-        JFrame frame = new JFrame("");
+        JFrame frame = new JFrame("Scenery Project");
+
+        //ask the user for time of day and season
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Welcome to my scenery picture.\nPlease input day or night: ");
+        String timeOfDay = sc.next();
+        if (timeOfDay.equalsIgnoreCase("day")){
+
+        }
+        else if (timeOfDay.equalsIgnoreCase("night")){
+
+        }
+        else{
+            System.out.println("Bad input.");
+        }
 
         //Create the JPanel object and add it to the frame
-        Scenery canvas = new Scenery();
+        Scenery canvas = new Scenery(timeOfDay);
         frame.add(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-
     }
 }
--- projects/q1project/Scenery.java
+++ projects/q1project/Scenery.java
@@ -4,16 +4,22 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Dimension;
 
+public class Scenery extends JPanel {	
 
-public class Scenery extends JPanel {
-	
-	public Scenery() {
+	//instance variables
+	private String timeOfDay;
+	private Color blue, green, yellow;
+
+	public Scenery(String timeOfDay) {
         setFocusable(true); // make sure focus is in this JPanel. This will become more important when we start using buttons.
         setLayout(null);    // setting to null allows you to control the layout of the JPanel.
         
         // add any initialization code to the constructor
+		this.timeOfDay = timeOfDay;
+		blue = new Color(0,0,255);
+		green = new Color(0,255,0);
+		yellow = new Color(255,255,0);
 
-		
 	}
 
 	@Override
@@ -23,19 +29,28 @@ public class Scenery extends JPanel {
 	}
 
 
-        /* Call all of your drawing methods from paintComponent(Graphics). You must pass the Graphics reference variable, g, to your
-           draw methods. */
+    /* Call all of your drawing methods from paintComponent(Graphics). You must pass the Graphics reference variable, g, to your
+    draw methods. */
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);   // DO NOT REMOVE THIS LINE
 
 		// Create a method for each item that you draw
-		drawSomething(g);
-
+		drawBackground(g);
 	}
 
-        // Make methods that are just called from within this class private
-	private void drawSomething(Graphics g) {
-		// need the graphics object to invoke the draw methods
+    // Make methods that are just called from within this class private
+	private void drawBackground(Graphics g) { // need the graphics object to invoke the draw methods
+		//draw either a day or night background and update as appropriate for the season
+		if (timeOfDay.equalsIgnoreCase("day")){
+			g.setColor(blue);
+			g.fillRect(0,0, 800, 350);
+			g.setColor(yellow);
+			g.fillOval(100, 50, 75, 75);
+		}else{
+
+		}
+		g.setColor(green);
+		g.fillRect(0,350, 800, 250);
 	}
 }
